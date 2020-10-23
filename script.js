72e71c66b058b410fe387064d551637e7135b0bc
Wednesday <yensonyu@gmail.com>
2020-10-22 23:30:47 -0700

    new update to js file

--- script.js
+++ script.js
@@ -4,13 +4,44 @@ var letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "1234567890";
 var characters = "!#$%&'()*+,-./:;<=>?@\[^_`{|}~";
 
-var lengthChoice = prompt("Please choose the length of password you want to generate. It should be between 8 - 128 characters:");
-var lowerCase = confirm("Do you want to include letters with lowercase?");
-var upperCase = confirm("Do you want to include letters with uppercase?");
-var numeric = confirm("Do you want to include the numbers?")
-var specialChar = confirm("Do you want to include the special characters?");
+// function generatePassword() {
+//   var typeChoice = "";
 
-function generatePassword() {
+//   if (lowerCase === true) {
+//     typeChoice = typeChoice + letter.toLowerCase();
+//   }
+
+//   if (upperCase === true) {
+//     typeChoice = typeChoice + letter;
+//   }
+
+//   if (numeric === true) {
+//     typeChoice = typeChoice + number;
+//   }
+
+//   if (specialChar === true) {
+//     typeChoice = typeChoice + characters;
+//   }
+// }
+
+
+// Write password to the #password input
+function writePassword() {
+  //var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  var password = "";
+
+  var lengthChoice = prompt("Please choose the length of password you want to generate. It should be between 8 - 128 characters:");
+  if ( lengthChoice < 8 || lengthChoice > 128) {
+    alert("Oooops! Not the length standard for password. Please try again.");
+    return password;
+  }
+
+  var lowerCase = confirm("Do you want to include letters with lowercase?");
+  var upperCase = confirm("Do you want to include letters with uppercase?");
+  var numeric = confirm("Do you want to include the numbers?")
+  var specialChar = confirm("Do you want to include the special characters?");
+
   var typeChoice = "";
 
   if (lowerCase === true) {
@@ -28,23 +59,9 @@ function generatePassword() {
   if (specialChar === true) {
     typeChoice = typeChoice + characters;
   }
-}
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var password = "";
 
   for (var i = 0; i < lengthChoice; i++) {
-    if (lowerCase === true) {
-      password = password + letter.toLowerCase().charAt(Math.floor(Math.random()*25));
-    }
-
-    if (upperCase === true) {
-      password = password + letter.charAt(Math.floor(Math.random()*25));
-    }
+    password = password + typeChoice[Math.floor(Math.random()*typeChoice.length)];
   }
 
   passwordText.value = password;
